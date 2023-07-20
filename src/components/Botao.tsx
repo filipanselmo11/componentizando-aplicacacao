9ff4d2c0eb5f1c9915402cde52244484e6bd3a3d
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2023-07-20 18:59:52 -0300

    feat: Concluído

--- src/components/Botao.tsx
+++ src/components/Botao.tsx
@@ -14,6 +14,7 @@ export function Botao({ iconName, title, selected, ...rest}: PropsBotao) {
     return (
         <button type="button" {...(selected && { className: 'selected'})} {...rest}>
             <Icone name={iconName} color={selected ? '#FAE800' : '#FBFBFB'}/>
+            {title}
         </button>
     );
 }
